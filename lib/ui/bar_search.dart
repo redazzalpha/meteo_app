72bc7f64ef162572bd97b18bad4c11cbfc7b8a68
kyzer <redazzkillah@gmail.com>
2023-12-02 13:41:11 +0100

    update sliver header bar

--- lib/ui/bar_search.dart
+++ lib/ui/bar_search.dart
@@ -11,7 +11,7 @@ class _BarSearchState extends State<BarSearch> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(50),
       child: SearchAnchor(
           builder: (BuildContext context, SearchController controller) {
         return SearchBar(
--- lib/ui/sliver_header_bar.dart
+++ lib/ui/sliver_header_bar.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class SliverHeaderBar extends StatelessWidget {
@@ -11,21 +13,40 @@ class SliverHeaderBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
+      automaticallyImplyLeading: false,
       primary: true,
-      title: const Text("sliver app bar title"),
-      centerTitle: true,
+      title: const Text("météo"),
       pinned: true,
+      toolbarHeight: 100,
       expandedHeight: 200,
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.transparent,
       bottom: PreferredSize(
         preferredSize: const Size.fromHeight(35.0),
         child: bottom ?? const SizedBox(),
       ),
-      flexibleSpace: const FlexibleSpaceBar(
-        centerTitle: true,
-        expandedTitleScale: 1.3,
-        background: Text("flexiblespacebar"),
-      ),
+      flexibleSpace: FlexibleSpaceBar(
+          centerTitle: true,
+          expandedTitleScale: 1.3,
+          title: Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 0.5,
+                  sigmaY: 0.5,
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context)
+                        .colorScheme
+                        .background
+                        .withOpacity(0.95),
+                  ),
+                ),
+              ),
+              const Text("météo"),
+            ],
+          )),
     );
   }
 }
